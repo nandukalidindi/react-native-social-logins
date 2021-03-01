0eddd63eab8ed63580435fe70e9a3464e729ac2c
Nandu Kalidindi <nandukalidindi@gmail.com>
2021-02-28 19:39:11 -0500

    Add all necessary packages to kickstart Twitter Login

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { WebView } from 'react-native-webview';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 0.5}}>
+      <WebView source={{ uri: 'https://reactnative.dev/' }} />
     </View>
   );
 }
