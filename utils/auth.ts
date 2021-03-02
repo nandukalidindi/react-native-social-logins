c0228cd7de1060d0e8869b2486e293811c22d5b2
Nandu Kalidindi <nandukalidindi@gmail.com>
2021-03-01 20:39:34 -0500

    Initial commit for setting up OAuth utils for 1.0 and 2.0

---
+++ utils/auth.ts
@@ -0,0 +1,12 @@
+export const objectToQueryString = (object: any, delimiter = ','): string => {
+    return Object.keys(object).map(key => `${key}=${object[key]}`).join(delimiter);
+};
+
+export const queryStringToObject = (queryString, separator = '&'): any => {
+    return queryString.split(separator).reduce((acc, param) => {
+        const [key, value] = param.split('=');
+        
+        acc[key] = value;
+        return acc;
+    }, {});
+}
